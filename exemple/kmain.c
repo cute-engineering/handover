366d5927dbdac184e421e950ed9e1eba43b0a290
VAN BOSSUYT Nicolas <nicolas.van.bossuyt@gmail.com>
2022-06-26 00:16:39 +0200

    update exemple.

--- exemple/kmain.c
+++ exemple/kmain.c
@@ -1,14 +1,12 @@
 #define HANDOVER_INCLUDE_MACROS
-#define HANDOVER_INCLUDE_UTILITIES
 #define HANDOVER_INCLUDE_HEADER
 
-#include "../handover.h"
+#include "handover.h"
 
 void ho_printf(void *ctx, char const *format, ...);
 
 HANDOVER(WITH_FB, WITH_FILES);
 
-void _kmain(uint64_t magic, Handover *handover)
+void _kmain(uint64_t magic, HandoverPayload *payload)
 {
-    handover_dump(handover, ho_printf, NULL);
 }
