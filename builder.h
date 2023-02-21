5b5975986ca0f48c2c2fd6153b09b58f21c67a3a
Keyboard Slayer <dalcjor07@gmail.com>
2023-02-21 14:31:54 +0100

    fix: removed records on payload (causes page faults)

--- builder.h
+++ builder.h
@@ -10,7 +10,6 @@ typedef struct
     size_t size;
     char *string;
     HandoverPayload *payload;
-    HandoverRecord *records;
 } HandoverBuilder;
 
 static inline void handover_builder_init(HandoverBuilder *builder, void *buf, size_t size)
@@ -23,13 +22,12 @@ static inline void handover_builder_init(HandoverBuilder *builder, void *buf, si
         .size = size,
         .string = ((char *)buf) + size,
         .payload = payload,
-        .records = builder->payload->records,
     };
 }
 
 static inline void handover_builder_append(HandoverBuilder *builder, HandoverRecord record)
 {
-    builder->records[builder->payload->count++] = record;
+    builder->payload->records[builder->payload->count++] = record;
 }
 
 static inline size_t handover_builder_append_str(HandoverBuilder *builder, const char *str)
@@ -37,7 +35,6 @@ static inline size_t handover_builder_append_str(HandoverBuilder *builder, const
     size_t len = strlen(str) + 1;
     builder->string -= len;
     memcpy(builder->string, str, len);
-    builder->string[len] = 0;
     return builder->string - (char *)builder->buf;
 }
 
